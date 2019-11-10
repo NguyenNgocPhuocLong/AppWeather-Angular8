48770a3f54d6071fe324f7f2348de237eb011aa1
NguyenNgocPhuocLong <nguyenngocphuoclong1994@gmail.com>
2019-11-10 18:31:35 +0700

    update angular PWA and Server-Side Rendering

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,10 +12,11 @@ import { WeatherCardComponent } from './shared/components/weather-card/weather-c
 import { AddCardComponent } from './shared/components/add-card/add-card.component';
 import { ErrorComponent } from './shared/components/error/error.component';
 import { DetailsComponent } from './pages/details/details.component';
-import { environment } from 'src/environments/environment';
 import { LoginComponent } from './pages/login/login.component';
 import { SignupComponent } from './pages/signup/signup.component';
 import { AddComponent } from './pages/add/add.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -30,12 +31,13 @@ import { AddComponent } from './pages/add/add.component';
     AddComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
     AppRoutingModule,
     FormsModule,
     HttpClientModule,
     NguiAutoCompleteModule,
-    AngularFireLite.forRoot(environment.config)
+    AngularFireLite.forRoot(environment.config),
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
